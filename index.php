bb10ae68dbe1966627bfc8c49d962d9d36cb86f7
jnoggle <justin.noggle@gmail.com>
2016-11-12 17:50:23 -0600

    Delete index.html

--- index.php
+++ index.php
@@ -1,16 +1,17 @@
 <?php
-include ('functions.php');
-include ('header.html');
+include ('run.html');
+// include ('functions.php');
+// include ('header.html');
 
-echo createPresetChords();
+// echo createPresetChords();
 
-echo '<br><br><br>';
+// echo '<br><br><br>';
 
-echo createNoteDropDown("n1"). " ";
-echo createNoteDropDown("n2"). " ";
-echo createNoteDropDown("n3"). " ";
-echo createNoteDropDown("n4"). "<br>";
+// echo createNoteDropDown("n1"). " ";
+// echo createNoteDropDown("n2"). " ";
+// echo createNoteDropDown("n3"). " ";
+// echo createNoteDropDown("n4"). "<br>";
 
 
-include ('footer.html');
+// include ('footer.html');
 ?>
